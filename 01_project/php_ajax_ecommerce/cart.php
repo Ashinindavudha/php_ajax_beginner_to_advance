8edd6d879914b7544d720d27ee932b44e983b105
Ashinindavudha <ashinindavudha2018@gmail.com>
2021-03-02 21:27:16 +0630

    add_main_js

--- 01_project/php_ajax_ecommerce/cart.php
+++ 01_project/php_ajax_ecommerce/cart.php
@@ -55,6 +55,7 @@
         </div>
       </nav>
     </header>
+    
 
     <div class="container mt-3">
       <main role="main">
@@ -104,4 +105,18 @@
     <script src="js/wow.min.js"></script>
     <script src="js/cart.js"></script>
   </body>
+</html>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>lesson 3</title>
+</head>
+<body>
+  <h1>Home Page</h1>
+  <hr>
+  <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Natus nulla ratione distinctio, quia cupiditate dignissimos at numquam doloremque dolore delectus ducimus libero sint. Vel ipsa repellat, hic voluptatibus eligendi deserunt!</p>
+</body>
 </html>
--- 01_project/php_ajax_ecommerce/js/main.js
+++ 01_project/php_ajax_ecommerce/js/main.js
@@ -28,3 +28,126 @@ var SALAD = [
   { name: "Salad #3", price: 15 },
 ];
 //html
+function HTMLfruitProduct(con) {
+  let URL = `img/fruits/fruit${con}.jpeg`;
+  let btn = `btnFruits${con}`;
+  return `
+  <div class="col-md-4 wow zoomIn data-wow-duration="10s" data-wow-offset="240" ">
+    <div class="card mb-4 shadow-sm">
+    <img src="${URL}" class="card-img-top" style="height: 16rem;" alt="Card image cap">
+   <div class="card-body">
+    <i style="color: orange;" class="fa fa-star"></i>
+    <i style="color: orange;" class="fa fa-star"></i>
+    <i style="color: orange;" class="fa fa-star"></i>
+    <i style="color: orange;" class="fa fa-star"></i>
+    <i style="color: orange;" class="fa fa-star"></i>
+<p class="card-text">${FRUIT[con - 1].name}</p>
+<p class="card-text">Price: $${FRUIT[con - 1].price}.00</p>
+<div class="d-flex justify-content-between align-items-center">
+<div class="btn-group">
+<button type="button" onclick="cart2('${FRUIT[con - 1].name}', '${
+    FRUIT[con - 1].price
+  }', '${URL}', '${con}', '${btn}')" class="btn btn-sm btn-ouline-secondary">
+<a href="cart.php" style="color: inherit;">Buy</a>
+</button>
+
+<button type="button" onclick="cart('${FRUIT[con - 1].name}', '${
+    FRUIT[con - 1].price
+  }', '${URL}', '${con}', '${btn}')" class="btn btn-sm btn-ouline-secondary">
+<a href="#" style="color: inherit;">Add To Card</a>
+</button>
+</div>
+<small class="text-muted">Free Shopping</small>
+</div>
+</div>
+</div>
+</div>
+  `;
+}
+
+function HTMLjuiceProduct(con) {
+  let URL = `img/juice/juice${con}.jpeg`;
+  let btn = `btnJuices${con}`;
+  return `
+  <div class="col-md-4 wow zoomIn data-wow-duration="10s" data-wow-offset="240" ">
+    <div class="card mb-4 shadow-sm">
+    <img src="${URL}" class="card-img-top" style="height: 16rem;" alt="Card image cap">
+   <div class="card-body">
+    <i style="color: orange;" class="fa fa-star"></i>
+    <i style="color: orange;" class="fa fa-star"></i>
+    <i style="color: orange;" class="fa fa-star"></i>
+    <i style="color: orange;" class="fa fa-star"></i>
+    <i style="color: orange;" class="fa fa-star"></i>
+<p class="card-text">${JUICE[con - 1].name}</p>
+<p class="card-text">Price: $${JUICE[con - 1].price}.00</p>
+<div class="d-flex justify-content-between align-items-center">
+<div class="btn-group">
+<button type="button" onclick="cart2('${JUICE[con - 1].name}', '${
+    JUICE[con - 1].price
+  }', '${URL}', '${con}', '${btn}')" class="btn btn-sm btn-ouline-secondary">
+<a href="cart.html" style="color: inherit;">Buy</a>
+</button>
+
+<button type="button" onclick="cart('${JUICE[con - 1].name}', '${
+    JUICE[con - 1].price
+  }', '${URL}', '${con}', '${btn}')" class="btn btn-sm btn-ouline-secondary">
+<a href="#" style="color: inherit;">Add To Card</a>
+</button>
+</div>
+<small class="text-muted">Free Shopping</small>
+</div>
+</div>
+</div>
+</div>
+  `;
+}
+
+function HTMLsaladProduct(con) {
+  let URL = `img/salads/salad${con}.jpeg`;
+  let btn = `btnSalads${con}`;
+  return `
+  <div class="col-md-4 wow zoomIn data-wow-duration="10s" data-wow-offset="240" ">
+    <div class="card mb-4 shadow-sm">
+    <img src="${URL}" class="card-img-top" style="height: 16rem;" alt="Card image cap">
+   <div class="card-body">
+    <i style="color: orange;" class="fa fa-star"></i>
+    <i style="color: orange;" class="fa fa-star"></i>
+    <i style="color: orange;" class="fa fa-star"></i>
+    <i style="color: orange;" class="fa fa-star"></i>
+    <i style="color: orange;" class="fa fa-star"></i>
+<p class="card-text">${SALAD[con - 1].name}</p>
+<p class="card-text">Price: $${SALAD[con - 1].price}.00</p>
+<div class="d-flex justify-content-between align-items-center">
+<div class="btn-group">
+<button type="button" onclick="cart2('${SALAD[con - 1].name}', '${
+    SALAD[con - 1].price
+  }', '${URL}', '${con}', '${btn}')" class="btn btn-sm btn-ouline-secondary">
+<a href="cart.html" style="color: inherit;">Buy</a>
+</button>
+
+<button type="button" onclick="cart('${SALAD[con - 1].name}', '${
+    SALAD[con - 1].price
+  }', '${URL}', '${con}', '${btn}')" class="btn btn-sm btn-ouline-secondary">
+<a href="#" style="color: inherit;">Add To Card</a>
+</button>
+</div>
+<small class="text-muted">Free Shopping</small>
+</div>
+</div>
+</div>
+</div>
+  `;
+}
+// ANIMATION
+function animation() {
+  const toast = swal.mixin({
+    toast: true,
+    position: "top-end",
+    showConfirmButton: false,
+    timer: 1000,
+  });
+  toast({
+    type: "success",
+    title: "Added to Shopping Cart",
+  });
+}
