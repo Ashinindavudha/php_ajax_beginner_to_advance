11c6ab21fc39300d3cb26a0863f92b0e0d2c5375
Ashinindavudha <ashinindavudha2018@gmail.com>
2021-03-07 16:28:50 +0630

    project_02_update

--- 02_project_php_jquery_ajax_crud/form.php
+++ 02_project_php_jquery_ajax_crud/form.php
@@ -15,12 +15,50 @@
         <label for="recipient-name" class="col-form-label">Name</label>
         <div class="input-group mb-3">
         <div class="input-group-prepend">
-        <span class=""></span>
+        <span class="input-group-text" id="basic-addon1"><i class="fa fa-user-circle-o" aria-hidden="true"></i></span>
+        </div>
+        <input type="text" class="form-control" id="username" name="username" requireed="required">
+        </div>
+        </div>
+        <div class="form-group">
+        <label for="recipient-name" class="col-form-label">Email</label>
+        <div class="input-group mb-3">
+        <div class="input-group-prepend">
+        <span class="input-group-text" id="basic-addon1"><i class="fa fa-envelope-o" aria-hidden="true"></i></span>
+        </div>
+        <input type="text" class="form-control" id="email" name="email" requireed="required">
+        </div>
+        </div>
+        <div class="form-group">
+        <label for="recipient-name" class="col-form-label">Phone</label>
+        <div class="input-group mb-3">
+        <div class="input-group-prepend">
+        <span class="input-group-text" id="basic-addon1"><i class="fa fa-phone" aria-hidden="true"></i></span>
+        </div>
+        <input type="text" class="form-control" id="username" name="phone" requireed="required" maxlength="11">
+        </div>
+        </div>
+        <div class="form-group">
+        <label for="recipient-name" class="col-form-label">User Photo</label>
+        <div class="input-group mb-3">
+        <div class="input-group-prepend">
+        <span class="input-group-text" id="basic-addon1"><i class="fa fa-picture-o" aria-hidden="true"></i></span>
+        </div>
+        <div class="custom-file">
+        <input type="file" class="custom-file-input" name="photo" id="userphoto">
+        <label class="custom-file-label" for="userphoto">Choose Photo</label>
         </div>
         </div>
         </div>
+    </div>
+    <div class="modal-footer">
+        <button type="button" class="btn btn-danger" data-dismiss="modal">Close</button>
+        <button type="submit" class="btn btn-success" id="addButton">Submit</button>
+        <input type="hidden" name="action" value="adduser">
+        <input type="hidden" name="userid" id="userid" value="">
     </div>
 </form>
 </div>
 </div>
 </div>
+<!-- add / edit form modal end -->
--- 02_project_php_jquery_ajax_crud/includes/Player.php
+++ 02_project_php_jquery_ajax_crud/includes/Player.php
@@ -1,19 +1,139 @@
 <?php
-class Database 
+require_once 'Database.php';
+class Player extends Database 
 {
-    private $dbServer = 'localhost';
-    private $dbUser = 'root';
-    private $dbPassword = '';
-    private $dbName = 'playersdb';
-    protected $conn;
-
-    public function __construct() {
+    // table name
+    protected $tableName = 'players';
+    public function add ($data) {
+        if (!empty($data)) {
+            $fileds = $placholders = [];
+            foreach ($data as $fileds => $value) {
+                $fileds[] = $fileds;
+                $placholders[] = ":{$fileds}";
+            }
+        }
+        $sql = "INSERT INTO {$this->tableName} (". implode(',', $fileds).") VALUES (" . implode(',', $placholders) .")";
+        $stmt = $this->conn->prepar($sql);
         try {
-            $dsn = "mysql:host={dbServer}; dbname={$this->dbName}; charset=utf8";
-            $options = array(PDO::ATTR_PERSISTENT);
-            $this->conn = new PDO($dsn, $this->dbUser, $this->dbPassword, $options);
+            $this->conn->beginTransaction();
+            $stmt->execute($data);
+            $lastInsertedId = $this->conn->lastInsertId();
+            $this->conn->commit();
+            return $lastInsertedId;
         } catch (PDOException $e) {
-            echo "Connection Error:" . $e->getMessage();
+            echo "Error:" . $e->getMessage();
+            $this->conn->rollback();
+        }
+    }
+
+    public function update($data, $id) {
+        if (!empty($data)) {
+            $fileds = '';
+            $x = 1;
+            $filedsCount = count($data);
+            foreach ($data as $filed => $value) {
+                $fileds .= "{$filed}=:{$filed}";
+                if ($x < $filedsCount) {
+                    $fileds .= ", ";
+                }
+                $x++;
+            }
+        }
+        $sql = "UPDATE {$this->tableName} SET {$fileds} WHERE id=:id";
+        $stmt = $this->conn->prepare($sql);
+        try {
+            $this->conn->beginTransaction();
+            $data['id'] = $id;
+            $stmt->execute($data);
+            $this->conn->commmit();
+        } catch (PDOException $e) {
+            echo "Error:" .$e->getMessage();
+            $this->conn->rollback();
+        }
+    }
+    public function getRows ($start = 0, $limit = 4) 
+    {
+        $sql = "SELECT * FROM {$this->tableName} ORDER BY id DESC LIMIT {$start}, {$limit}";
+        $stmt = $this->conn->prepare($sql);
+        $stmt->execute();
+        if ($stmt->rowCount() > 0) {
+           $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+        } else {
+            $results = [];
+        }
+        return $results;
+    }
+
+    // delete row using id
+    public function deleteRow($id)
+    {
+        $sql = "DELETE FROM {$this->tableName} WHERE id=:id";
+        $stmt = $this->conn->prepare($sql);
+        try {
+            $stmt->execute([':id' => $id]);
+            if ($stmt->rowCount() > 0) {
+                return true;
+            }
+        } catch (PDOException $e) {
+            echo "Error:" .$e->getMessage();
+            return false;
+        }
+    }
+    public function getCount()
+    {
+        $sql = "SELECT count(*) as pcount FROM {$this->tableName}";
+        $stmt = $this->conn->prepare($sql);
+        $stmt->execute();
+        $result = $stmt->fetch(PDO::FETCH_ASSOC);
+        return $result['pcount'];
+    }
+
+    public function getRow($filed, $value)
+    {
+        $sql = "SELECT * FROM {$this->tableName} WHERE {$filed}=:{$filed}";
+        $stmt = $this->conn->prepare($sql);
+        $stmt->execute([":{$filed}" => $value]);
+        if ($stmt->rowCount() > 0) {
+            $result = $stmt->fetch(PDO::FETCH_ASSOC);
+
+        } else {
+          $result = [];
+
+        }
+        return $result;
+    }
+    public function searchPlayer($searchText, $start = 0, $limit = 4)
+    {
+        $sql = "SELECT * FROM {$this->tableName} WHERE pname LIKE :search ORDER BY id DESC LIMIT {$start}, {$limit}";
+        $stmt = $this->conn->prepare($sql);
+        $stmt->execute([':search' => "{searchText}%"]);
+        if ($stmt->rowCount() > 0) {
+            $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+        } else {
+            $results = [];
+        }
+        return $results;
+    }
+
+    public function uploadPhoto($file)
+    {
+        if (!empty($file)) {
+            $fileTemPath = $file['tmp_name'];
+            $fileName = $file['name'];
+            $fileSize = $file['size'];
+            $fileType = $file['type'];
+            $fileNameCmps = explode('.', $fileName);
+            $fileExtension = strtolower(end($fileNameCmps));
+            $newFileName = md5(time() . $fileName). '.' . $fileExtension;
+            $allowedExtn = ["jpg", "png", "gif", "jpeg"];
+            if (in_array($fileExtension, $allowedExtn)) {
+                $uploadFileDir = getcwd() . '/uploads';
+                $destFilePath = $uploadFileDir . $newFileName;
+                if (move_uploaded_file($fileTemPath, $destFilePath)) {
+                   return $newFileName;
+                }
+            }
         }
     }
 }
